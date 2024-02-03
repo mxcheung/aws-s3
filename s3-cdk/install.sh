4158605d30a7627653980ddba4250835d1d88d3e
mxcheung <mxcheung@users.noreply.github.com>
2024-02-03 11:11:03 +1100

    Create install.sh

---
+++ s3-cdk/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Creates s3 bucket via cdk
+
+echo "Welcome aws s3"
+
+cd /home/ec2-user/environment/aws-s3/s3-cdk
+
+cdk init app --language typescript
+
+npm install @aws-cdk/aws-dynamodb @aws-cdk/aws-lambda @aws-cdk/aws-apigateway @aws-cdk/core aws-sdk @aws-cdk/aws-iam
+
+
+cdk bootstrap
+
+cdk synth
+
+cdk deploy
