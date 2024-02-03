72ce0d1e3b10a46dc39f690a901cd0fb0f63aab4
mxcheung <mxcheung@users.noreply.github.com>
2024-02-03 11:14:53 +1100

    Update s3-cdk-stack.ts

--- s3-cdk/lib/s3-cdk-stack.ts
+++ s3-cdk/lib/s3-cdk-stack.ts
@@ -1,1 +1,16 @@
-s3-cdk-stack.ts
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class S3CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'S3CdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
