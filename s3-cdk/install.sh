97b714134636112d82e699572acadcb7f612492f
mxcheung <mxcheung@users.noreply.github.com>
2024-02-03 11:51:35 +1100

    Update install.sh

--- s3-cdk/install.sh
+++ s3-cdk/install.sh
@@ -7,7 +7,7 @@ cd /home/ec2-user/environment/aws-s3/s3-cdk
 
 cdk init app --language typescript
 
-npm install @aws-cdk/aws-dynamodb @aws-cdk/aws-lambda @aws-cdk/aws-apigateway @aws-cdk/core aws-sdk @aws-cdk/aws-iam
+npm install @aws-cdk/aws-s3 @aws-cdk/aws-dynamodb @aws-cdk/aws-lambda @aws-cdk/aws-apigateway @aws-cdk/core aws-sdk @aws-cdk/aws-iam
 
 
 cdk bootstrap
