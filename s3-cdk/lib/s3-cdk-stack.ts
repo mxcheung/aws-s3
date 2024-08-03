4ebcb9677cb3aa0b4fbfc34611ef401abbc9ea33
mxcheung <mxcheung@users.noreply.github.com>
2024-08-03 18:33:03 +1000

    Update s3-cdk-stack.ts

--- s3-cdk/lib/s3-cdk-stack.ts
+++ s3-cdk/lib/s3-cdk-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import { RemovalPolicy, Stack, StackProps, Duration } from 'aws-cdk-lib';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class S3CdkStack extends cdk.Stack {
@@ -44,7 +45,7 @@ export class S3CdkStack extends cdk.Stack {
     // Deploy files to the bucket
     new s3deploy.BucketDeployment(this, 'DeployFiles', {
       sources: [s3deploy.Source.asset('./data-folder')],
-      destinationBucket: myBucket,
+      destinationBucket: s3Bucket,
     });
     
   }
