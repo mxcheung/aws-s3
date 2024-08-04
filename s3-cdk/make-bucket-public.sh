22d3a121f07eb0fe76c3d8b090f9da37d9b41d50
mxcheung <mxcheung@users.noreply.github.com>
2024-08-04 21:39:20 +1000

    Update and rename make-bucket-public to make-bucket-public.sh

--- s3-cdk/make-bucket-public.sh
+++ s3-cdk/make-bucket-public.sh
@@ -3,16 +3,19 @@
 # Define the bucket name
 # bucket_name="your-bucket-name"
 
-aws s3api list-buckets --query "Buckets[].Name"
-
+# Iterate over each bucket
 for bucket in $(aws s3api list-buckets --query "Buckets[].Name" --output text)
 do
-  tags=$(aws s3api get-bucket-tagging --bucket $bucket_name --query "TagSet[?Key=='Department' && Value=='Marketing']")
-  if [ ! -z "$tags" ]; then
+  # Get the tags for the current bucket
+  tags=$(aws s3api get-bucket-tagging --bucket $bucket --query "TagSet[?Key=='Department' && Value=='Marketing']" --output json)
+
+  # Check if the tags array is not empty
+  if [ "$(echo "$tags" | jq length)" -gt 0 ]; then
+    bucket_name=$bucket 
     echo "Bucket with Department=Marketing: $bucket"
   fi
 done
-
+echo $bucket_name
 
 
 # Create a bucket policy JSON content
