fd4947fcd4167277855a4771169067a5edab63c3
mxcheung <mxcheung@users.noreply.github.com>
2024-10-31 13:02:46 +1100

    Update set_up.sh

--- presign_url/lambda/set_up.sh
+++ presign_url/lambda/set_up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export S3_BUCKET_NAME="S3TriggerLambdaBucketQaisar"
+export S3_BUCKET_NAME="s3triggerlambdabucketqaisar"
 export LAMBDA_FUNCTION_NAME="S3TriggerLambda"
 export LAMBDA_ROLE_NAME="LambdaS3ExecutionRole"
 
