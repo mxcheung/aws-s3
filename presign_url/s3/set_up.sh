c2b0ecd1dd0f1c57de21d32ccf14ca3416de0d8a
mxcheung <mxcheung@users.noreply.github.com>
2024-10-31 16:24:51 +1100

    Update set_up.sh

--- presign_url/s3/set_up.sh
+++ presign_url/s3/set_up.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-
-export S3_BUCKET_NAME="s3triggerlambdabucketqaisar"
+BASE_BUCKET_NAME="s3triggerlambdabucketqaisar"
+UNIQUE_SUFFIX=$(date +%Y%m%d%H%M%S)  # Generate a unique suffix based on current timestamp
+export S3_BUCKET_NAME="${BASE_BUCKET_NAME}${UNIQUE_SUFFIX}"  # Concatenate to create a unique bucket name
 
 aws s3api create-bucket \
   --bucket $S3_BUCKET_NAME
