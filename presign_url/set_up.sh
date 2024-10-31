c644f0f2090bc8b1d18ba3639c52585d01c8e7a6
mxcheung <mxcheung@users.noreply.github.com>
2024-10-31 12:27:49 +1100

    Update set_up.sh

--- presign_url/set_up.sh
+++ presign_url/set_up.sh
@@ -6,10 +6,10 @@ cd /home/ec2-user/aws-s3/presign_url/user_credentials
 cd /home/ec2-user/aws-s3/presign_url/iam
 . ./set_up.sh
 
-cd /home/ec2-useraws-s3/presign_url/s3
+cd /home/ec2-user/aws-s3/presign_url/s3
 . ./set_up.sh
 
-cd /home/ec2-useraws-s3/presign_url/lambda
+cd /home/ec2-user/aws-s3/presign_url/lambda
 . ./set_up.sh
 
 
