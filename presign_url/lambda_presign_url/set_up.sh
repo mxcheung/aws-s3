c480ed4adf76f1bc4650d1609e66ca836cc6ccbf
mxcheung <mxcheung@users.noreply.github.com>
2024-10-31 16:27:46 +1100

    Update set_up.sh

--- presign_url/lambda_presign_url/set_up.sh
+++ presign_url/lambda_presign_url/set_up.sh
@@ -4,6 +4,15 @@ export S3_BUCKET_NAME="s3triggerlambdabucketqaisar"
 export LAMBDA_FUNCTION_NAME="S3PresignURLLambda"
 export LAMBDA_ROLE_NAME="LambdaS3ExecutionRole"
 
+# Variables
+BASE_BUCKET_NAME="s3triggerlambdabucket"
+REGION="us-east-1"                                  # Replace with your AWS region
+
+# Get the existing S3 bucket name based on the base name
+export S3_BUCKET_NAME=$(aws s3api list-buckets --query "Buckets[?starts_with(Name, \`${BASE_BUCKET_NAME}\`)].Name" --output text)
+
+
+
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 
 zip function.zip lambda_function.py
