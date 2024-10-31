bc2c8c153169a3a8b39356920a1bdf8475540652
mxcheung <mxcheung@users.noreply.github.com>
2024-10-31 12:26:24 +1100

    Update set_up.sh

--- presign_url/set_up.sh
+++ presign_url/set_up.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd /home/ec2-user/aws-cloudwatch/expiration_poll/user_credentials
+cd /home/ec2-user/aws-s3/presign_url/user_credentials
 . ./set_up.sh
 
-cd /home/ec2-user/aws-cloudwatch/expiration_poll/iam
+cd /home/ec2-user/aws-s3/presign_url/iam
 . ./set_up.sh
 
-cd /home/ec2-user/aws-cloudwatch/expiration_poll/s3
+cd /home/ec2-useraws-s3/presign_url/s3
 . ./set_up.sh
 
-cd /home/ec2-user/aws-cloudwatch/expiration_poll/lambda
+cd /home/ec2-useraws-s3/presign_url/lambda
 . ./set_up.sh
 
 
