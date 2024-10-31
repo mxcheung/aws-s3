eafac4ad6ca93f9d4480c6c8d7edc0e1cd9e89bd
mxcheung <mxcheung@users.noreply.github.com>
2024-10-31 12:24:33 +1100

    Update set_up.sh

--- presign_url/set_up.sh
+++ presign_url/set_up.sh
@@ -6,6 +6,9 @@ cd /home/ec2-user/aws-cloudwatch/expiration_poll/user_credentials
 cd /home/ec2-user/aws-cloudwatch/expiration_poll/iam
 . ./set_up.sh
 
+cd /home/ec2-user/aws-cloudwatch/expiration_poll/s3
+. ./set_up.sh
+
 cd /home/ec2-user/aws-cloudwatch/expiration_poll/lambda
 . ./set_up.sh
 
