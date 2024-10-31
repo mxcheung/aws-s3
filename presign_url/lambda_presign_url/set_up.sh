6261843f13f06f5992219e7ed03d041a3e6729e2
mxcheung <mxcheung@users.noreply.github.com>
2024-10-31 14:16:02 +1100

    Create set_up.sh

---
+++ presign_url/lambda_presign_url/set_up.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export S3_BUCKET_NAME="s3triggerlambdabucketqaisar"
+export LAMBDA_FUNCTION_NAME="S3PresignURLLambda"
+export LAMBDA_ROLE_NAME="LambdaS3ExecutionRole"
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+
+zip function.zip lambda_function.py
+
+# Wait for the IAM role to be created
+aws iam wait role-exists --role-name $LAMBDA_ROLE_NAME
+
+export LAMBDA_ROLE_ARN=$(aws iam get-role --role-name $LAMBDA_ROLE_NAME --query 'Role.Arn' --output text)
+
+aws lambda create-function \
+    --function-name $LAMBDA_FUNCTION_NAME \
+    --zip-file fileb://function.zip \
+    --handler lambda_function.lambda_handler \
+    --runtime python3.9 \
+    --role $LAMBDA_ROLE_ARN \
