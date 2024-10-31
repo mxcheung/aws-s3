4075172e61d17042442b06c80da8d5d01a811a9c
mxcheung <mxcheung@users.noreply.github.com>
2024-10-31 12:45:30 +1100

    Update set_up.sh

--- presign_url/s3_notification/set_up.sh
+++ presign_url/s3_notification/set_up.sh
@@ -5,6 +5,7 @@ export LAMBDA_FUNCTION_NAME="S3TriggerLambda"
 export LAMBDA_ROLE_NAME="LambdaS3ExecutionRole"
 
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+REGION=$(aws configure get region)
 
 
 aws s3api put-bucket-notification-configuration \
