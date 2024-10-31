919296b353eca05b108d0bd5179bec1fb88edb8d
mxcheung <mxcheung@users.noreply.github.com>
2024-10-31 14:29:52 +1100

    Update set_up.sh

--- presign_url/s3_test_upload/set_up.sh
+++ presign_url/s3_test_upload/set_up.sh
@@ -6,7 +6,6 @@ export LAMBDA_ROLE_NAME="LambdaS3ExecutionRole"
 
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 
-
 # aws s3 cp ./test-upload.txt s3://$S3_BUCKET_NAME/
 
 
