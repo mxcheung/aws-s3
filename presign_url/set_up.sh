ac859daed24a1fca92e003f463711a206bf36979
mxcheung <mxcheung@users.noreply.github.com>
2024-10-31 12:08:37 +1100

    Update set_up.sh

--- presign_url/set_up.sh
+++ presign_url/set_up.sh
@@ -3,19 +3,10 @@
 cd /home/ec2-user/aws-cloudwatch/expiration_poll/user_credentials
 . ./set_up.sh
 
-cd /home/ec2-user/aws-cloudwatch/expiration_poll/dynamodb
-. ./set_up.sh
-
 cd /home/ec2-user/aws-cloudwatch/expiration_poll/iam
-. ./set_up.sh
-
-cd /home/ec2-user/aws-cloudwatch/expiration_poll/sns
 . ./set_up.sh
 
 cd /home/ec2-user/aws-cloudwatch/expiration_poll/lambda
 . ./set_up.sh
 
 
-cd /home/ec2-user/aws-cloudwatch/expiration_poll/data_load
-chmod +x dynamodb_dataload.sh
-. ./parallel_nohup.sh
