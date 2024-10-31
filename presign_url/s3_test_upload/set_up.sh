9bc1660787440e27d64a810249500c1de155e844
mxcheung <mxcheung@users.noreply.github.com>
2024-10-31 16:29:57 +1100

    Update set_up.sh

--- presign_url/s3_test_upload/set_up.sh
+++ presign_url/s3_test_upload/set_up.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-export S3_BUCKET_NAME="s3triggerlambdabucketqaisar"
 export LAMBDA_FUNCTION_NAME="S3PresignURLLambda"
 export LAMBDA_ROLE_NAME="LambdaS3ExecutionRole"
 
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+
+# Get the existing S3 bucket name based on the base name
+export S3_BUCKET_NAME=$(aws s3api list-buckets --query "Buckets[?starts_with(Name, \`${BASE_BUCKET_NAME}\`)].Name" --output text)
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+REGION=$(aws configure get region)
 
 # aws s3 cp ./test-upload.txt s3://$S3_BUCKET_NAME/
 
