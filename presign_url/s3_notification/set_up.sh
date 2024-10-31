e20713f4c59638fd4dc99370d89c9c539ab7f8a6
mxcheung <mxcheung@users.noreply.github.com>
2024-10-31 12:40:38 +1100

    Create set_up.sh

---
+++ presign_url/s3_notification/set_up.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export S3_BUCKET_NAME="S3TriggerLambdaBucketQaisar"
+export LAMBDA_FUNCTION_NAME="S3TriggerLambda"
+export LAMBDA_ROLE_NAME="LambdaS3ExecutionRole"
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
